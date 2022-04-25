60e4a4df882457b95a7d65e613f353ce76793779
An2sBRR <84623865+An2sBRR@users.noreply.github.com>
2022-04-25 18:51:28 +0200

    Add files via upload
    Squelette HTML + mise en forme avec CSS
    A rajouter : 
    - connexion + panier
    - contact en bas
    - régler les images
    - rajouter menu horizontal dans header
    - faire les liens vers les autres pages

--- index.php
+++ index.php
@@ -3,9 +3,33 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/index.css" />
     <title>PHP - Hello, World!</title>
 </head>
-<body>
-        <h1><?php echo 'Hello, World!'; ?></h1>
+<body >
+<div class="imageH">
+            <img src="/projet/img/logo.png" alt="logo"  width="90" height="90">
+</div>
+        <header>
+            <h1>SITE POUR FAIRE DU BIFFFF</h1>
+
+        </header>
+        <div class="menuG">
+            <div class="menu">
+        <h1>Menu :</h1>
+        <ul>
+            <li>Accueil</li>
+            <li>Catalogue 1</li>
+            <li>Catalogue 2</li>
+            <li>Catalogue 3</li>
+            <li>Contact</li>
+        </ul>
+</div>
+        </div>
+        <aside>
+        <img src="/projet/img/illustration.png" alt="illustration" width="350" height="350">
+
+        </aside>
+        <footer>okok</footer>
 </body>
 </html>
