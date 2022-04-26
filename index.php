0bc8c2d24706368be0a4c01c8bdaccd9c16133f9
An2sBRR <84623865+An2sBRR@users.noreply.github.com>
2022-04-26 19:27:03 +0200

    Amélioration de l'affichage

--- index.php
+++ index.php
@@ -6,30 +6,44 @@
     <link rel="stylesheet" href="css/index.css" />
     <title>PHP - Hello, World!</title>
 </head>
-<body >
-<div class="imageH">
-            <img src="/projet/img/logo.png" alt="logo"  width="90" height="90">
-</div>
-        <header>
-            <h1>SITE POUR FAIRE DU BIFFFF</h1>
+<body>
+       
+    <header>
+    <div class="nul">
+       <h1>SITE POUR FAIRE DU BIFFFF </h1> 
+       <div class="menuH">
+                <ul>
+                    <li><a href="#">Accueil</a></li>
+                    <li>Catalogue 1</li>
+                    <li>Catalogue 2</li>
+                    <li>Catalogue 3</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+            </div>
+        <div class="conex">Connexion <br><br> Panier</div>
+       <!--  <img src="/projet/img/logo.png" alt="logo" id="logo" width="90" height="90">-->
+    </header>
+        <div class="milieu">
+       <nav>
+          <div class="menu">
+                <h1>Menu :</h1>
+                <ul>
+                    <li>Accueil</li>
+                    <li>Catalogue 1</li>
+                    <li>Catalogue 2</li>
+                    <li>Catalogue 3</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+        </nav> 
+        <main> 
+         ouiuo
+       <!-- <img src="/projet/img/illustration.png" alt="illustration" width="350" height="350">-->
+    </main>
+    </div>
+    
 
-        </header>
-        <div class="menuG">
-            <div class="menu">
-        <h1>Menu :</h1>
-        <ul>
-            <li>Accueil</li>
-            <li>Catalogue 1</li>
-            <li>Catalogue 2</li>
-            <li>Catalogue 3</li>
-            <li>Contact</li>
-        </ul>
-</div>
-        </div>
-        <aside>
-        <img src="/projet/img/illustration.png" alt="illustration" width="350" height="350">
-
-        </aside>
-        <footer>okok</footer>
+    <footer><h1>okok</h1></footer>
 </body>
 </html>
