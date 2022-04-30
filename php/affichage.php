19b0fcf7ad99ea917af9e47c98c99f62c57f7786
Le_sept_ou_le_siete <76405539+Le-7@users.noreply.github.com>
2022-05-01 00:50:51 +0200

    Partie fonctionnelle fine

--- php/affichage.php
+++ php/affichage.php
@@ -9,6 +9,14 @@ function getquery(){ $url = $_SERVER['REQUEST_URI'];
 function affichage(){
 $num = explode('=',getquery());
 echo $num[1];
+$csv = array_map('str_getcsv', file('../data/exemple.csv'));
+echo $csv;
+foreach($csv as $line){
+    if($line[4] ==$num[1] ){
+        //do what ever you want
+        var_dump($line[0]);
+    }
+}
 }
 affichage();
 ?>
