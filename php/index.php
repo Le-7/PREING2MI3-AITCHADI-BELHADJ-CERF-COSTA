a560d45420b7f2d0fb54c477ec8d23884e05ba9b
An2sBRR <84623865+An2sBRR@users.noreply.github.com>
2022-04-30 23:15:20 +0200

    Changements de mise en forme/visuel

---
+++ php/index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/index.css" />
+    <title>PHP - Hello, World!</title>
+</head>
+<body>
+    <div class="contenu">   
+        <header>
+            <div class="nvo">
+                <img src="/projet/img/logo.jpg" alt="logo" id="logo">
+            </div>
+
+            <div class="nul">
+                <div class="conex">
+                <div class="connexion">Connexion</div> 
+                <br>
+                <div class="panier">Panier</div>
+                </div>
+
+                <h1>SITE POUR FAIRE DU BIFFFF </h1> 
+                <div class="menuH">
+                    <ul>
+                        <li><a href="#">Accueil</a></li>
+                        <li>Catalogue 1</li>
+                        <li>Catalogue 2</li>
+                        <li>Catalogue 3</li>                        
+                        <li>Contact</li>
+                    </ul>
+                </div>
+            </div>
+        </header>
+    
+        <div class="milieu">
+                <nav>
+                    <div class="menu">
+                        <h1>Menu :</h1>
+                        <ul>
+                            <li>Accueil</li>
+                            <li>Catalogue 1</li>
+                            <li>Catalogue 2</li>
+                            <li>Catalogue 3</li>
+                            <li>Contact</li>
+                        </ul>
+                    </div>
+                </nav> 
+                <main>     
+                    <img src="/projet/img/illustration.png" alt="illustration" width="350" height="350">
+                </main>
+        </div>
+        
+        <footer>
+            <p style="font-size:20px">Société BusinessBiff</p>
+            <p style="font-size:18px">Mentions légales :</p>
+            <p style="font-size:18px">Contact : 06.77.88.92.99</p>   
+        </footer>
+    </div>
+</body>
+</html>
