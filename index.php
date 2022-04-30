3ecf46259fcb84de67529a5818142c31970919eb
An2sBRR <84623865+An2sBRR@users.noreply.github.com>
2022-04-30 23:03:45 +0200

    Changements de mise en forme/visuel

--- index.php
+++ index.php
@@ -7,57 +7,55 @@
     <title>PHP - Hello, World!</title>
 </head>
 <body>
-       
-    <header>
-
-        <div class="nvo">
-            <img src="/projet/img/logo.jpg" alt="logo" id="logo" >
-        </div>
-    <div class="nul">
-    <div class="conex">Connexion <br><br> Panier</div>
-    <h1>SITE POUR FAIRE DU BIFFFF </h1> 
-  
-    
-    
-       
-       <div class="menuH">
-                <ul>
-                    <li><a href="#">Accueil</a></li>
-                    <li>Catalogue 1</li>
-                    <li>Catalogue 2</li>
-                    <li>Catalogue 3</li>
-                    <li>Contact</li>
-                </ul>
+    <div class="contenu">   
+        <header>
+            <div class="nvo">
+                <img src="/projet/img/logo.jpg" alt="logo" id="logo">
             </div>
-</div>
-    
 
+            <div class="nul">
+                <div class="conex">
+                <div class="connexion">Connexion</div> 
+                <br>
+                <div class="panier">Panier</div>
+                </div>
+
+                <h1>SITE POUR FAIRE DU BIFFFF </h1> 
+                <div class="menuH">
+                    <ul>
+                        <li><a href="#">Accueil</a></li>
+                        <li>Catalogue 1</li>
+                        <li>Catalogue 2</li>
+                        <li>Catalogue 3</li>                        
+                        <li>Contact</li>
+                    </ul>
+                </div>
+            </div>
+        </header>
     
-        
-          
-       
-      
-    </header>
         <div class="milieu">
-       <nav>
-          <div class="menu">
-                <h1>Menu :</h1>
-                <ul>
-                    <li>Accueil</li>
-                    <li>Catalogue 1</li>
-                    <li>Catalogue 2</li>
-                    <li>Catalogue 3</li>
-                    <li>Contact</li>
-                </ul>
-            </div>
-        </nav> 
-        <main> 
-         ouiuo
-       <!-- <img src="/projet/img/illustration.png" alt="illustration" width="350" height="350">-->
-    </main>
+                <nav>
+                    <div class="menu">
+                        <h1>Menu :</h1>
+                        <ul>
+                            <li>Accueil</li>
+                            <li>Catalogue 1</li>
+                            <li>Catalogue 2</li>
+                            <li>Catalogue 3</li>
+                            <li>Contact</li>
+                        </ul>
+                    </div>
+                </nav> 
+                <main>     
+                    <img src="/projet/img/illustration.png" alt="illustration" width="350" height="350">
+                </main>
+        </div>
+        
+        <footer>
+            <p style="font-size:20px">Société BusinessBiff</p>
+            <p style="font-size:18px">Mentions légales :</p>
+            <p style="font-size:18px">Contact : 06.77.88.92.99</p>   
+        </footer>
     </div>
-    
-
-    <footer><h1>okok</h1></footer>
 </body>
 </html>
