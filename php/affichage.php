de2b6fe73b56b570c2ccba11fa128a51805b1da5
Le_sept_ou_le_siete <76405539+Le-7@users.noreply.github.com>
2022-05-01 16:19:10 +0200

    parti fonctionnel fini(reste a personnalisé en fonction du csv)

--- php/affichage.php
+++ php/affichage.php
@@ -8,13 +8,10 @@ function getquery(){ $url = $_SERVER['REQUEST_URI'];
 
 function affichage(){
 $num = explode('=',getquery());
-echo $num[1];
 $csv = array_map('str_getcsv', file('../data/exemple.csv'));
-echo $csv;
 foreach($csv as $line){
     if($line[4] ==$num[1] ){
-        //do what ever you want
-        var_dump($line[0]);
+        echo("<h1>".$line[0]."</h1>");
     }
 }
 }
