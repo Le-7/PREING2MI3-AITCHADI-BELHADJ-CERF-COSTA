e07ef62f9260722330a293f90dae16dc148a425d
An2sBRR <84623865+An2sBRR@users.noreply.github.com>
2022-06-03 09:36:21 +0200

    Update index.php

--- index.php
+++ index.php
@@ -76,14 +76,16 @@
                     <a class="reseauxlog" href="https://instagram.com" target="_blank">
                         <ion-icon name="logo-instagram"></ion-icon>
                     </a>
-                        <a class="reseauxlog" href="https://github.com" target="_blank">
+                        <a class="reseauxlog" href="https://github.com/Le-7/Projet-Info-Preing2" target="_blank">
                     <ion-icon name="logo-github"></ion-icon>
                     </a>
                 </p>  
             </div>
 
             <div class="infos" id="contact2">
-                <p style="font-size:22px">couturalia@society.com</p>  
+                <p style="font-size:22px">
+                 <a href="mailto:couturalia@society.com?subject=Demande%20de%20contact&body=Bonjour,"> 
+                couturalia@society.com </a></p>  
             </div>  
 
             <div class="infos" id="mentions">
