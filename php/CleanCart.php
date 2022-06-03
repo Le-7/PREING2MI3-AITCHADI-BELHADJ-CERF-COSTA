c230e87cc39a54fb9e22bf8b51f7f3451de98690
Fab780 <104725129+Fab780@users.noreply.github.com>
2022-06-03 17:42:09 +0200

    Delete CleanCart.php

--- php/CleanCart.php
+++
@@ -1,5 +0,0 @@
-<?php 
-    session_start();
-    $_SESSION['cart'] = [ 'm01' => 0 , 'm02' => 0 , 'm03' => 0 , 'i01' => 0 , 'i02' => 0 , 'i03' => 0 , 't01' => 0 , 't02' => 0 , 't03' => 0 ];
-    header('Location: ' . $_SERVER['HTTP_REFERER']);
-?>
