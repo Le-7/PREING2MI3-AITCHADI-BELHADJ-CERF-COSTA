810be2173bced6c1b0687b686b3b6fa41cb43dea
An2sBRR <84623865+An2sBRR@users.noreply.github.com>
2022-06-03 21:56:27 +0200

    remise après fichiers supp

--- php/contacter.php
+++ php/contacter.php
@@ -3,30 +3,34 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/index.css" />
+    <link rel="stylesheet" href="../css/contacter.css" />
     <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Pacifico" />
     <title>Test données</title>
 </head>
 <body>
 
     <?php
-        $demande = file_get_contents('contacter.json') ;
-        $demande = json_decode($demande, true) ;
+        $demande = file_get_contents('contacter.json') ; #on récupère les données du json
+        $demande = json_decode($demande, true) ; #on remet en php
 
-        for($i=0; $i<count($demande); $i++) :
+        for($i=0; $i<count($demande); $i++) : #on fait une boucle jusqu'à ce que tous les éléments soient lus puis on les écrit
     ?>
         <div class="test">
-            <div class="contenu">
+            <div class="zones">
+                <!-- Lien sur le "bouton" x pour supprimer le message sélectionné -->
+            <a href="traitement.php?del=<?php echo$demande[$i]['id']; ?>" class="action" id="fermé">X</a> <br>
+            <p>
                 <b><?php echo$demande[$i]['nom']; ?></b><br>
                 <?php echo$demande[$i]['email']; ?><br>
                 <?php echo$demande[$i]['tel']; ?><br>
-                <p>
-                    <?php echo$demande[$i]['message']; ?>
-                </p>
-                <b class="date"> <?php echo$demande[$i]['date']; ?></b>
+                
+                    <?php echo$demande[$i]['message']; ?> <br>
+                
+                <b class="date"> <?php echo$demande[$i]['date']; ?></b><br> </p>
+                
             </div>
-            <!-- -->
-                <a href="traitement.php?del=<?php echo$demande[$i]['id']; ?>" class="action">X</a> 
+            
+               
 
         </div>
     <?php endfor; ?>
--- php/traitement.php
+++ php/traitement.php
@@ -1,38 +1,59 @@
 <?php
-    if(isset($_POST['nom'])){
+    if(isset($_POST['nom'])){  
 
         $demande = array() ;
         $demande['nom'] = $_POST['nom'] ;
         $demande['email'] = $_POST['email'] ;
+        $demande['objet'] = $_POST['objet'] ;
         $demande['tel'] = $_POST['tel'] ;
         $demande['message'] = $_POST['message'] ;
         $demande['date'] = date("d-m-Y  H:i") ;
-        $demande['id'] = date("dmYHis") ; #pour unique 
+        $demande['id'] = date("dmYHis") ; #pour un id unique par demande 
 
-        $json = file_get_contents('contacter.json') ; #var qui contient notre fichier json
-        #sous forme de tableau
+        //on reteste coté serveur
+        if(!preg_match("/^[a-zA-Z-' ]*$/",$demande['nom'])){
+            echo "<script language='Javascript'>window.alert('Erreur dans le nom (format non compatible)');</script>";
+            echo "<script language='Javascript'>document.location='http://localhost:8080/Projet-Info-Preing2-main/php/contact.php?cat=nom'</script>";
+            exit;
+        }
+        if(!filter_var($demande['email'], FILTER_VALIDATE_EMAIL)){
+            echo "<script language='Javascript'>window.alert('Erreur dans l email (format non compatible ex: lesept@gmail.com)');</script>";
+            echo "<script language='Javascript'>document.location='http://localhost:8080/Projet-Info-Preing2-main/php/contact.php?cat=email'</script>";
+            exit;
+        }
+        if(!preg_match("/^[0-9]{10}$/",$demande['tel'])){
+            echo "<script language='Javascript'>window.alert('Erreur dans le champs en rouge (format non compatible, numero a 10 chiffres accolés');</script>";
+            echo "<script language='Javascript'>document.location='http://localhost:8080/Projet-Info-Preing2-main/php/contact.php?cat=tel'</script>";
+            exit;
+        }
+        /*if(!preg_match("/^[a-zA-Z-']*$/",$demande['message'])){
+            echo "<script language='Javascript'>window.alert('Erreur dans le champs en rouge (texte');</script>";
+            echo "<script language='Javascript'>document.location='http://localhost:8080/Projet-Info-Preing2-main/php/contact.php?cat=message'</script>";
+            exit;
+        }       Si on veut rajouter un regex pour le message*/ 
+        $json = file_get_contents('contacter.json') ; #var qui contient notre fichier json sous forme de tableau
         $json = json_decode($json, true) ; #conversion en php
-        $json[] = $demande ;
+        $json[] = $demande ; #on rempli le tableau
         $json = json_encode($json) ; #reconversion en json
-        file_put_contents('contacter.json', $json) ; #on remet le premier msg dans le fichier json
-        header("location: ./") ; #renvoyer sur index.php apres envoi
-
+        file_put_contents('contacter.json', $json) ; #on remet le premier msg dans le fichier json puis on créé un mail prérempli avec les données saisies
+        echo ("<script language='Javascript'>document.location='mailto:couturalia@society.com?subject=".$demande['objet']."&body=".$demande['message']."%0A".$demande['nom']."%0ATéléphone :%20".$demande['tel']."'</script>");
 
     }
 
-    else if(isset($_GET['del'])){
-        $demande = file_get_contents('contacter.json');
-        $demande = json_decode($demande, true);
-
-        $verif = array() ;
+    else if(isset($_GET['del'])){  #Si on a del
+        $demande = file_get_contents('contacter.json');  #on récupère le fichier json
+        $demande = json_decode($demande, true);          #on le converti en php
+ 
+        $verif = array() ;    #création d'un deuxième tableau vide 
 
         for($i=0; $i<count($demande); $i++){
-            if($demande[$i]['id'] != $_GET['del']){ #si diff alors on le fait entrer dans le nvo tableau
+            if($demande[$i]['id'] != $_GET['del']){ #si diff alors on fait entrer le msg dans le nvo tableau
                 $verif[] = $demande[$i] ;
             } 
         
         }
-        $verif = json_encode($verif);
-        file_put_contents('contacter.json', $verif) ;
-        header("location : contacter.php");
+        $verif = json_encode($verif);   #on remet en json
+        file_put_contents('contacter.json', $verif) ;  #on remet le contenu du msg dans le json puis on réactualise la page
+        echo "<script language='Javascript'>document.location='http://localhost:8080/Projet-Info-Preing2-main/php/contacter.php'</script>";
     }
+?>
